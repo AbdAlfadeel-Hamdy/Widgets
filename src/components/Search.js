6ed8919b8c38eeab3814d0e9be6ab57c055a385f
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-08-09 02:42:42 +0200

    fixing a warning

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,14 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 const Search = () => {
   const [term, setTerm] = useState("programming");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+    return () => clearTimeout(timerId);
+  }, [term]);
   useEffect(() => {
     const search = async () => {
       const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
@@ -11,22 +18,13 @@ const Search = () => {
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
       setResults(data.query.search);
     };
-
-    if (term && !results.length) {
-      search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (!term) return;
-        search();
-      }, 1000);
-      return () => clearTimeout(timeoutId);
-    }
-  }, [term]);
+    search();
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => (
     <div key={result.pageid} className="item">
