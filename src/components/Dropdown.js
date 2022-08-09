ad9f32dc4b9f1be2e29242a676d498d3f34310ac
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-08-09 22:57:09 +0200

    refactoring

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -3,17 +3,19 @@ import React, { useEffect, useRef, useState } from "react";
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
   const ref = useRef();
+  const onBodyClick = (e) => {
+    if (ref.current.contains(e.target)) return;
+    setOpen(false);
+  };
   useEffect(() => {
-    document.body.addEventListener(
-      "click",
-      (e) => {
-        if (ref.current.contains(e.target)) return;
-        setOpen(false);
-      },
-      {
+    document.body.addEventListener("click", onBodyClick, {
+      capture: true,
+    });
+    return () => {
+      document.body.removeEventListener("click", onBodyClick, {
         capture: true,
-      }
-    );
+      });
+    };
   }, []);
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) return null;
