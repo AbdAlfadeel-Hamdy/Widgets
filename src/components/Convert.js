7d0a2f3a357f97668e3d192924fcf915881d9190
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-08-10 05:55:11 +0200

    Displaying the translated text

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,20 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 const Convert = ({ text, language }) => {
+  const [translated, setTranslated] = useState("");
   useEffect(() => {
-    axios.post(
-      "https://translation.googleapis.com/language/translate/v2",
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-          key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
-        },
-      }
-    );
-    console.log(language.value);
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+      console.log(data);
+    };
+    doTranslation();
   }, [text, language]);
+  return <h1 className="ui header">{translated}</h1>;
 };
 
 export default Convert;
